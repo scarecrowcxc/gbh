fb776079edc515b94f4c80b66dbb32da82c946e1
scarecrowcxc <45313440+scarecrowcxc@users.noreply.github.com>
2022-11-01 09:56:51 +0800

    create new file fibcalculator.py and insert code. (#2)

---
+++ fibcalculator.py
@@ -0,0 +1,18 @@
+class FibCalculator:
+    def __init__(self, cache=None):
+        if cache:
+            self.cache=cache
+        else:
+            self.cache={}
+
+            
+    def fib(self, n):
+        if n in self.cache:
+            return self.cache[n]
+        elif n <= 2:
+            return 1
+        else:
+            ret = self.fib(n-1) + self.fib(n-2)
+            self.cache[n] = ret
+            return ret
+
